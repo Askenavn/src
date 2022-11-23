9d1ba1a09059ec99d59176c30539eec877d52dea
nanzat <dashiev1301@gmail.com>
2022-11-23 22:36:21 +0700

    while was reworked, made new function playVideo

--- cpp_aruco_detect/video_capture/main.cpp
+++ cpp_aruco_detect/video_capture/main.cpp
@@ -8,15 +8,18 @@ public:
    std::array<int, 2> resolution;
    std::string winname;
    cv::VideoCapture cap;
+   bool success;
 
    Camera(int cam_id):
-      cap(cam_id){
+      cap(cam_id), success(false){
       cap.set(cv::CAP_PROP_FOURCC, ('M','P','E','G'));
 
       if (cap.isOpened() == false) {
          std::cout << "[LOG ]: Cannot open the video camera" << std::endl;
          return;
       }
+
+      success = true;
 
       double dWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH); 
       double dHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT); 
@@ -25,29 +28,36 @@ public:
 
    cv::Mat getFrame(){
       cv::Mat frame;
-      bool bSuccess = cap.read(frame); 
+      success = cap.read(frame); 
 
-      if (bSuccess == false) {
+      if (success == false) {
          std::cout << "[LOG ]: Video camera is disconnected" << std::endl;
       }
 
       return frame;
    }
 
+   bool playVideo(cv::Mat frame){
+      cv::imshow(winname, frame);
+
+      if (cv::waitKey(10) == 27){
+         std::cout << "[LOG ]: Esc key is pressed by user. Stopping the video" << std::endl;
+         return false;
+      }
+      
+      return true;
+   }
 };
 
 
 
 int main(int argc, char* argv[]){
    Camera camera(0);
-   while (true){
+   while (camera.success){
       cv::Mat frame = camera.getFrame();
 
-      cv::imshow(camera.winname, frame);
-
-         if (cv::waitKey(10) == 27){
-      std::cout << "[LOG ]: Esc key is pressed by user. Stoppig the video" << std::endl;
-      break;
+      if(!camera.playVideo(frame)){
+         break;
       }
    }
    return 0;
