760f3843e045c18d0e23f98b326bacb9e2e48c99
nanzat <dashiev1301@gmail.com>
2022-11-26 00:21:10 +0700

    new struct CameraParams added

--- cpp_aruco_detect/video_capture/main.cpp
+++ cpp_aruco_detect/video_capture/main.cpp
@@ -2,16 +2,26 @@
 #include <iostream>
 #include <array>
 
+ //новую структуру
+
+struct CameraParams{
+   int camId;
+   std::array<int, 2> resolution;
+   int FPS;
+
+   CameraParams(int id, int width, int height): 
+      camId(id), resolution({width, height}){
+   }
+};
 
 class Camera{
 public:
-   std::array<int, 2> resolution;
    std::string winname;
    cv::VideoCapture cap;
-   bool success;
+   bool isWorking;
 
-   Camera(int cam_id):
-      cap(cam_id), success(false){
+   Camera(CameraParams params):
+      cap(params.camId), isWorking(false){
       cap.set(cv::CAP_PROP_FOURCC, ('M','P','E','G'));
 
       if (cap.isOpened() == false) {
@@ -19,47 +29,80 @@ public:
          return;
       }
 
-      success = true;
+      isWorking = true;
 
-      double dWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH); 
+      cap.set(cv::CAP_PROP_FRAME_WIDTH, params.resolution[0]);
+      cap.set(cv::CAP_PROP_FRAME_HEIGHT, params.resolution[1]);
+
+      double dWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH); //удалить
       double dHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT); 
       std::cout << "[LOG ]: Resolution of the video : " << dWidth << " x " << dHeight << std::endl; 
    }
 
    cv::Mat getFrame(){
       cv::Mat frame;
-      success = cap.read(frame); 
+      isWorking = cap.read(frame); 
 
-      if (success == false) {
+      if (isWorking == false) {
          std::cout << "[LOG ]: Video camera is disconnected" << std::endl;
       }
 
       return frame;
    }
 
-   bool playVideo(cv::Mat frame){
-      cv::imshow(winname, frame);
+   CameraParams getCameraInfo(CameraParams a){
+      a.resolution[0] = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+      a.resolution[1] = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+      a.FPS = cap.get(cv::CAP_PROP_FPS);
 
-      if (cv::waitKey(10) == 27){
-         std::cout << "[LOG ]: Esc key is pressed by user. Stopping the video" << std::endl;
-         return false;
-      }
-      
-      return true;
+      return a; 
    }
+
+   void printResolution(CameraParams par){
+      std::cout << "[LOG ]: Cam" << par.camId << " Resolution of the video : " << par.resolution[0]
+       << " x " << par.resolution[1]  << std::endl; 
+   }
+
+   void printResolution(){
+      std::cout << "[LOG ]: Resolution of the video : " << cap.get(cv::CAP_PROP_FRAME_WIDTH)
+       << " x " << cap.get(cv::CAP_PROP_FRAME_HEIGHT)  << std::endl; 
+   }
+
+   void printFPS(CameraParams par){
+      std::cout<<"[LOG ]: Cam" << par.camId << " " << par.FPS  << " FPS" <<std::endl;
+   }
+
+   void printFPS(){
+      std::cout<<"[LOG ]: " << cap.get(cv::CAP_PROP_FPS) << " FPS" <<std::endl;
+   }
+
+
 };
 
 
 
 int main(int argc, char* argv[]){
-   Camera camera(0);
-   while (camera.success){
+   CameraParams par(2, 800, 600);
+   Camera camera(par);
+
+   camera.printResolution(par);
+   
+   while (camera.isWorking){
       cv::Mat frame = camera.getFrame();
 
-      if(!camera.playVideo(frame)){
+      camera.getCameraInfo(par);
+
+      camera.printFPS();
+     
+
+      cv::imshow(camera.winname, frame);
+
+      if (cv::waitKey(10) == 27){
+         std::cout << "[LOG ]: Esc key is pressed by user. Stopping the video" << std::endl;
          break;
       }
    }
+
    return 0;
 }
    
